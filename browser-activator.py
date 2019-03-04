91eb2d23e9361ec683a594ca86251d7a2b398aa7
Samir Sheriff <samiriff@gmail.com>
2019-03-03 23:55:09 -0800

    Made Browser Activator object-oriented and provided a provision to specify content path, which is the root directory of the saved html files

--- browser-activator.py
+++ browser-activator.py
@@ -3,60 +3,65 @@ import time
 import os.path
 import sys
 
-def executeCommand(cmd):
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-    stdout, stderr = p.communicate()
-    return stdout
+class BrowserActivator:
+    def __init__(self, contentPath):
+        self.contentPath = contentPath
+        self.firefoxPid = self.executeCommand('xdotool search --name "Mozilla Firefox"').strip()
 
-def getFileName(firefoxPid, fileExtension):
-    filename = executeCommand('xdotool getwindowname ' + str(firefoxPid)).strip()
-    filename = filename.split('-')[0].strip() + fileExtension
-    print(filename)
-    return filename
+    def executeCommand(self, cmd):
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        stdout, stderr = p.communicate()
+        return stdout
 
-def isSaveStarted(firefoxPid):
-    dirname = getFileName(firefoxPid, '_files')
-    return os.path.isdir(dirname)
+    def getFileName(self, fileExtension):
+        filename = self.executeCommand('xdotool getwindowname ' + str(self.firefoxPid)).strip()
+        filename = filename.split('-')[0].strip() + fileExtension
+        print(filename)
+        return filename
 
-def isSaveCompleted(firefoxPid):
-    filename = getFileName(firefoxPid, '.html')
-    return os.path.isfile(filename)
+    def isSaveStarted(self):
+        dirname = self.getFileName('_files')
+        return os.path.isdir(dirname)
 
-def getTitleUpdaterJSCode():
-    return '''t = setInterval(function() {document.title = document.getElementsByClassName('_item--item-selected--3LMMf')[0].children[0].title;}, 100);'''
+    def isSaveCompleted(self):
+        filename = self.getFileName('.html')
+        return os.path.isfile(self.contentPath + filename)
 
-def init(firefoxPid):
-    executeCommand('xdotool windowactivate ' + str(firefoxPid) + ' key --clearmodifiers "ctrl+shift+i"')
-    time.sleep(3)
-    print('xdotool windowactivate --sync ' + str(firefoxPid) + ' type "' + getTitleUpdaterJSCode() + '"')
-    executeCommand('xdotool windowactivate --sync ' + str(firefoxPid) + ' type "' + getTitleUpdaterJSCode() + '"')
-    executeCommand('xdotool windowactivate ' + str(firefoxPid) + ' key --clearmodifiers "Return"')
-    time.sleep(3)
-    executeCommand('xdotool windowactivate ' + str(firefoxPid) + ' key --clearmodifiers "ctrl+shift+i"')
-    time.sleep(3)
+    def getTitleUpdaterJSCode(self):
+        return '''t = setInterval(function() {document.title = document.getElementsByClassName('_item--item-selected--3LMMf')[0].children[0].title;}, 100);'''
 
-def perform():    
-    firefoxPid = executeCommand('xdotool search --name "Mozilla Firefox"').strip()
-    print(firefoxPid)
-    init(firefoxPid)
-    inProgress = False
-    while(True):
-        if inProgress:            
-            print("Saving in progress")
-            if isSaveCompleted(firefoxPid):
-	        print("File saved")
-	        inProgress = False
+    def init(self):
+        self.executeCommand('xdotool windowactivate ' + str(self.firefoxPid) + ' key --clearmodifiers "ctrl+shift+i"')
+        time.sleep(3)
+        print('xdotool windowactivate --sync ' + str(self.firefoxPid) + ' type "' + self.getTitleUpdaterJSCode() + '"')
+        self.executeCommand('xdotool windowactivate --sync ' + str(self.firefoxPid) + ' type "' + self.getTitleUpdaterJSCode() + '"')
+        self.executeCommand('xdotool windowactivate ' + str(self.firefoxPid) + ' key --clearmodifiers "Return"')
+        time.sleep(3)
+        self.executeCommand('xdotool windowactivate ' + str(self.firefoxPid) + ' key --clearmodifiers "ctrl+shift+i"')
+        time.sleep(3)
+
+    def perform(self):
+        print(self.firefoxPid)
+        self.init()
+        inProgress = False
+        while(True):
+            if inProgress:
+                print("Saving in progress")
+                if self.isSaveCompleted():
+                    print("File saved")
+                    inProgress = False
+                else:
+                    time.sleep(1)
             else:
+                print("Beginning Save")
+                time.sleep(5)
+                self.executeCommand('xdotool windowactivate ' + str(self.firefoxPid) + ' key --clearmodifiers "ctrl+s"')
                 time.sleep(1)
-        else:
-            print("Beginning Save")
-            time.sleep(5)
-            executeCommand('xdotool windowactivate ' + str(firefoxPid) + ' key --clearmodifiers "ctrl+s"')
-            time.sleep(1)
-	    executeCommand('save_as=$(xdotool search --name "Save As") && xdotool windowactivate $save_as key --clearmodifiers "Return"')
-	    time.sleep(2)
-            inProgress = True
+                self.executeCommand('save_as=$(xdotool search --name "Save As") && xdotool windowactivate $save_as key --clearmodifiers "Return"')
+                time.sleep(2)
+                inProgress = True
 
 if __name__ == '__main__':
     key = raw_input("Press Enter Key to begin")
-    perform()
+    browserActivator = BrowserActivator('../Content/')
+    browserActivator.perform()
